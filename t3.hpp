bab0b2560fc89cb6ae5250fa737379197f38bfaa
666cgg <141202476+666cgg@users.noreply.github.com>
2024-09-22 21:03:12 +0800

    Add files via upload

--- t3.hpp
+++ t3.hpp
@@ -198,7 +198,6 @@ private:
             params.emplace("s",s);
             auto res=cl.Post(path,params);
             if(res) {
-
                 return res->body;
             }else {
                 return "连接失败";
@@ -393,10 +392,10 @@ private:
             }
             throw std::runtime_error("JSON文件缺少'xinxi'键或其不是一个对象: " + filename);
     }
-    static int a14(const std::string&url,const std::string&path,const std::string&outputFile) {
+        static int a14(const std::string&url,const std::string&path,const std::string&outputFile) {
             return a15(url,path,outputFile);
         }
-    static bool a15(const std::string& url,const std::string&path, const std::string& outputFile) {
+         static bool a15(const std::string& url,const std::string&path, const std::string& outputFile) {
             httplib::Client cli(url);
             auto res = cli.Get(path);
             if (res) {
@@ -426,28 +425,54 @@ public:
     static int get_id() {
         return a3();
     }
-
-    static int fastboot_clean_userdata(const std::string &id) {
+    static std::basic_string<char> fastboot_clean_userdata(const std::string &id) {
         return a4(id);
     }
-    static int unlock_bootloader(const std::string &id) {
+    static std::basic_string<char> unlock_bootloader(const std::string &id) {
         return a5(id);
     }
-    static int flash_boot(const std::string &id,const std::string&path) {
+    static std::basic_string<char> flash_boot(const std::string &id, const std::string&path) {
         return a6(id,path);
     }
-    static int flash_init_boot(const std::string &id,const std::string&path){
+    static std::basic_string<char> flash_init_boot(const std::string &id, const std::string&path){
         return a7(id,path);
     }
     static std::string kernel_version(const std::string &id){
         return a8(id);
     }
+    static std::basic_string<char> run_shell(const std::string&path){
+        return a9(path);
+    }
+    static int all_file(const std::string &directoryPath){
+        return a10(directoryPath);
+    }
+    static std::basic_string<char> transmit(const std::string &path1, const std::string &path2){
+        return a11(path1,path2);
+    }
+    static std::basic_string<char> adb_reboot_bootloader(const std::string&id){
+        return a12(id);
+    }
+    static std::basic_string<char> fastboot_reboot(const std::string&id){
+        return a13(id);
+    }
+    static std::basic_string<char> flash_boot_a(const std::string &id, const std::string&path){
+        return a14(id,path);
+    }
+    static std::basic_string<char> flash_boot_b(const std::string &id, const std::string&path){
+        return a15(id,path);
+    }
+    static std::basic_string<char> current_slot(){
+        return a16();
+    }
+
 private:
     static int a1(int a) {
        if(a==0){
-           system("adb devices");
+           std::string b=exec("adb devices");
+           std::cout<<b<<std::endl;
        }
-        system("fastboot devices");
+        std::string b=exec("fastboot devices");
+       std::cout<<b<<std::endl;
         return 0;
     }
     static std::string a2() {
@@ -455,39 +480,39 @@ private:
         return deviceId;
     }
     static int a3() {
-        system("adb devices");
+        exec("adb devices");
         return 0;
     }
     static std::string exec(const char* cmd) {
-        std::array<char, 128> buffer{};
+        std::array<char, 1024> buffer{};
         std::string result;
         std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
         if (!pipe) {
-            throw std::runtime_error("popen() failed!");
+            throw std::runtime_error("open() failed!");
         }
         while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
             result += buffer.data();
         }
         return result;
     }
-    static int a4(const std::string &id) {
+    static std::basic_string<char> a4(const std::string &id) {
         std::string a="fastboot -s "+id+" erase userdata";
-        auto b=system(a.c_str());
+        auto b=exec(a.c_str());
         return b;
     }
-    static int a5(const std::string &id) {
+    static std::basic_string<char> a5(const std::string &id) {
         std::string a="fastboot -s "+id+" oem unlock";
-        auto b=system(a.c_str());
+        auto b=exec(a.c_str());
         return b;
     }
-    static int a6(const std::string &id,const std::string&path) {
+    static std::basic_string<char> a6(const std::string &id, const std::string&path) {
         std::string a="fastboot -s "+id+" flash boot "+path;
-        auto b =system(a.c_str());
+        auto b =exec(a.c_str());
         return b;
     }
-    static int a7(const std::string &id,const std::string&path){
+    static std::basic_string<char> a7(const std::string &id, const std::string&path){
         std::string a="fastboot -s "+id+" flash init_boot "+path;
-        auto b =system(a.c_str());
+        auto b =exec(a.c_str());
         return b;
     }
     static std::string a8(const std::string &id){
@@ -495,5 +520,45 @@ private:
         auto b=exec(a.c_str());
         return b;
     }
-
+    static std::basic_string<char> a9(const std::string &path){
+        std::string a="."+path;
+        auto b=exec(a.c_str());
+        return b;
+    }
+    static int a10(const std::string &directoryPath){
+        for (const auto &entry : fs::directory_iterator(directoryPath)) {
+            std::cout << entry.path().filename() << std::endl;
+        }
+        return 0;
+    }
+    static std::basic_string<char> a11(const std::string &path1, const std::string &path2){
+        std::string a="dd if="+path1+" of="+path2;
+        auto b=exec(a.c_str());
+        return b;
+    }
+    static std::basic_string<char> a12(const std::string&id){
+        std::string a="adb -s "+id+" reboot bootloader";
+        auto b=exec(a.c_str());
+        return b;
+    }
+    static std::string a13(const std::string&id){
+        std::string a="fastboot -s "+id+" reboot";
+        auto b=exec(a.c_str());
+        return b;
+    }
+    static std::basic_string<char> a14(const std::string &id, const std::string&path) {
+        std::string a="fastboot -s "+id+" flash boot_a "+path;
+        auto b =exec(a.c_str());
+        return b;
+    }
+    static std::basic_string<char> a15(const std::string &id, const std::string&path) {
+        std::string a="fastboot -s "+id+" flash boot_b "+path;
+        auto b =exec(a.c_str());
+        return b;
+    }
+    static std::basic_string<char> a16(){
+        std::string a="fastboot getvar current-slot";
+        auto b =exec(a.c_str());
+        return b;
+    }
 };
